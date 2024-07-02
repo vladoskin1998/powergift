99560ff1619b70fed2f54a6c82fbc6d31108dd87
vladosik1998 <vladosik4891@gmail.com>
2024-07-02 21:23:23 +0300

    fix background catalog product

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,42 @@
 /// <reference types="react-scripts" />
   // "homepage": "https://vladoskin1998.github.io/powergift/",
+
+
+  // // отримання підкатегорій всіх
+
+  // id
+  // name
+  // categoriesName
+  // categoriesId
+  // fileLink
+
+  // // отримання підкатегорії
+
+  // id
+  // files: [
+  //   {
+  //     fileName,
+  //     colors,
+  //     idColors
+  //   }
+  // ]
+  // article
+  // brand
+  // company
+  // name
+  // price
+  // numbersOfProducy
+  // avaliableOfProducts
+  // country
+  // categorie
+  // size
+  // materials: [
+  //   idMaterials
+  //   valueMaterials
+  // ]
+  // characteristics: [
+  //   {"key": "value"}
+  // ]
+  // description
+
+//отриму
